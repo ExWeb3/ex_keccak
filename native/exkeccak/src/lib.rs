c6987d8b3ca0020ddc23b06316b5033e73cb6650
Ayrat Badykov <ayratin555@gmail.com>
2020-09-07 13:58:26 +0300

    do not fail on invalid type
    Currently, `ExKeccak.hash_256/1` raises an exception when invalid data
    is provided. This PR changes error handling logic. Now `{:error,
    :invalid_type}` will be returned if the provided data is not binary

--- native/exkeccak/src/lib.rs
+++ native/exkeccak/src/lib.rs
@@ -1,12 +1,13 @@
-use rustler::{Env, Error, Term};
 use rustler::types::binary::{Binary, OwnedBinary};
 use rustler::types::Encoder;
+use rustler::{Env, Term};
 use tiny_keccak::{Hasher, Keccak};
 
 mod atoms {
     rustler::rustler_atoms! {
         atom ok;
         atom error;
+        atom invalid_type;
     }
 }
 
@@ -18,8 +19,11 @@ rustler::rustler_export_nifs! {
     None
 }
 
-fn hash_256<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let data: Binary = args[0].decode()?;
+fn hash_256<'a>(env: Env<'a>, args: &[Term<'a>]) -> Term<'a> {
+    let data: Binary = match args[0].decode() {
+        Ok(binary) => binary,
+        Err(_error) => return (atoms::error(), atoms::invalid_type()).encode(env),
+    };
     let data_slice = data.as_slice();
 
     let mut keccak = Keccak::v256();
@@ -32,5 +36,5 @@ fn hash_256<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let mut erl_bin: OwnedBinary = OwnedBinary::new(32).unwrap();
     erl_bin.as_mut_slice().copy_from_slice(&result);
 
-    Ok((atoms::ok(), erl_bin.release(env)).encode(env))
+    (atoms::ok(), erl_bin.release(env)).encode(env)
 }
