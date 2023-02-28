708782e44be739281e1afa451b85ebf65b0bfb60
Razvan Draghici <tzumby@gmail.com>
2023-02-28 10:20:34 -0500

    Bringing in rustler-precompiled

--- lib/ex_keccak.ex
+++ lib/ex_keccak.ex
@@ -1,9 +1,16 @@
 defmodule ExKeccak do
+  version = Mix.Project.config()[:version]
+
   @external_resource "README.md"
   @moduledoc "README.md"
              |> File.read!()
 
-  use Rustler, otp_app: :ex_keccak, crate: :exkeccak, path: "native/exkeccak"
+  use RustlerPrecompiled,
+    otp_app: :ex_keccak,
+    crate: "exkeccak",
+    base_url: "https://github.com/tzumby/ex_keccak/releases/download/v#{version}",
+    force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
+    version: version
 
   def hash_256(_data), do: :erlang.nif_error(:nif_not_loaded)
 end
--- mix.exs
+++ mix.exs
@@ -1,16 +1,19 @@
 defmodule ExKeccak.MixProject do
   use Mix.Project
 
+  @version "0.7.0"
+  @source_url "https://github.com/tzumby/ex_keccak"
+
   def project do
     [
       app: :ex_keccak,
-      version: "0.6.0",
+      version: @version,
       elixir: "~> 1.10",
       description: description(),
       compilers: Mix.compilers(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      source_url: "https://github.com/tzumby/ex_keccak",
+      source_url: @source_url,
       package: package(),
       deps: deps(),
       name: "ExKeccek"
@@ -22,13 +25,11 @@ defmodule ExKeccak.MixProject do
       maintainers: ["tzumby"],
       name: "ex_keccak",
       licenses: ["Apache License 2.0"],
-      links: %{"GitHub" => "https://github.com/tzumby/ex_keccak"},
+      links: %{"GitHub" => @source_url},
       files: [
         "mix.exs",
-        "native/exkeccak/src",
-        "native/exkeccak/.cargo/config",
-        "native/exkeccak/Cargo.toml",
-        "native/exkeccak/Cargo.lock",
+        "native",
+        "checksum-*.exs",
         "lib",
         "LICENSE",
         "README.md",
@@ -53,7 +54,8 @@ defmodule ExKeccak.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:benchee, "~> 1.0", only: :test},
-      {:rustler, "~> 0.26"}
+      {:rustler, "~> 0.27"},
+      {:rustler_precompiled, "~> 0.6.1"}
     ]
   end
 end
