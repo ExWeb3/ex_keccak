b39b759c3836becf99076092a0e0b22f1fd43b37
Razvan Draghici <tzumby@gmail.com>
2023-02-28 10:23:33 -0500

    Adds targets to rustler precompiled configuration

--- lib/ex_keccak.ex
+++ lib/ex_keccak.ex
@@ -10,6 +10,8 @@ defmodule ExKeccak do
     crate: "exkeccak",
     base_url: "https://github.com/tzumby/ex_keccak/releases/download/v#{version}",
     force_build: System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"],
+    targets:
+      Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
     version: version
 
   def hash_256(_data), do: :erlang.nif_error(:nif_not_loaded)
