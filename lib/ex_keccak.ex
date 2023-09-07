4717c9f1b984b07898e093aeac0f6b5ebbea7b01
Ayrat Badykov <ayratin555@gmail.com>
2023-09-07 15:53:49 +0300

    add support for freebsd (#40)
    * add support for freebsd
    
    - https://github.com/philss/rustler_precompiled/pull/55
    -
    https://github.com/elixir-explorer/explorer/blob/main/.github/workflows/release.yml#L41
    
    * update action
    
    * add freebsd to the list of targets

--- lib/ex_keccak.ex
+++ lib/ex_keccak.ex
@@ -13,7 +13,10 @@ defmodule ExKeccak do
     base_url: "https://github.com/tzumby/ex_keccak/releases/download/v#{version}",
     force_build: System.get_env("EXKECCAK_BUILD") in ["1", true] or env_config[:ex_keccak],
     targets:
-      Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
+      Enum.uniq(
+        ["aarch64-unknown-linux-musl", "x86_64-unknown-freebsd"] ++
+          RustlerPrecompiled.Config.default_targets()
+      ),
     version: version
 
   def hash_256(_data), do: :erlang.nif_error(:nif_not_loaded)
